e77873a779436bfa088bf543835058d196ff0267
Natasha Chernis <natashachernis@gmail.com>
2019-03-11 05:45:48 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
     guess = gets.chomp.to_i
     if guess == random_num
       puts "You guessed the correct number!"
-    elsif guess != random_num
+    elsif guess > 0 && guess < 7
       puts "The computer guessed #{random_num}."
     else
       puts "Goodbye!"
