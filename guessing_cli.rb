9975c696fcc783c055b6a1ee11f7c1c2a916556d
Natasha Chernis <natashachernis@gmail.com>
2019-03-11 05:35:46 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,3 +1,12 @@
 def random_num_gen
   rand(1..6)
 end
+
+def run_guessing_game
+  puts "Guess a number between 1 and 6."
+  guess = gets.chomp
+  if guess == random_num_gen
+    puts "You guessed the correct number!"
+  else
+    puts "The computer guessed #{random_num_gen}."
+end
